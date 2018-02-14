3533a84cb66624af43d4a01dcef6e84e35cff5c0
samory harris <samory070@gmail.com>
2018-02-14 20:43:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = "I don't do much."
 kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  kittens.unshift()
+  kittens.unshift(name)
 }
 
 function destructivelyPrependKitten(name) {
-  kittens.push()
+  kittens.push(name)
 }
